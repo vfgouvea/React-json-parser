1c724c3f68dea830ff2abf79538149df0f6b573d
Vinicius <vfgouvea@gmail.com>
2020-04-19 16:58:46 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+   state = {
+        resp: "",
+        msg: ""
+   }
+
+   render() {
+        return (
+            <div className="upload-container">
+                <div className="upload-header">
+                    <h2>Json Parser Example</h2>
+                </div>
+                <div className="upload-content">
+                    <div className="single-upload">
+                        <h3>Upload Single File</h3>
+                        <div id="singleUploadForm" name="singleUploadForm">
+                            <input id="singleFileUploadInput" type="file" name="file" className="file-input" required />
+                            <button type="submit" className="primary submit-btn" onClick={this.submitFile}>Submit</button>
+                        </div>
+                        <div className="upload-response">
+                            <div id="singleFileUploadMsg">{this.state.msg}</div>
+                            <p id="resposta">{this.state.resp}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    submitFile = () => {
+        var files = document.querySelector('#singleFileUploadInput').files;
+        if(files.length !== 0) {
+           this.uploadSingleFile(files[0]);
+        }else{
+            this.setState({resp:"",msg: "Favor selecionar um arquivo!"});
+        }
+    }
+
+    uploadSingleFile(file) {
+
+        var formData = new FormData();
+        formData.append("file", file);
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open("POST", "http://localhost:8080/parseFile");
+
+        //depois da chamada
+        xhr.onload = function() {
+            var response = JSON.parse(xhr.responseText);
+            console.log(response);
+            if(xhr.status === 200) {
+                this.setState({resp:xhr.responseText,msg: "Arquivo lido corretamente"});
+            } else {
+                this.setState({resp:"",msg:(response && response.message) || "Ocorreu algum erro"});
+            }
+        }.bind(this);
+
+        xhr.onerror = function () {
+           this.setState({ resp:"",msg:"Problema na chamada para api" });
+        }.bind(this);
+
+        xhr.send(formData);
+    }
 }
 
 export default App;
