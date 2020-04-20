bd29c0774f1c65ed19fa2906882221786b9ad1f7
Vinicius <vfgouvea@gmail.com>
2020-04-20 17:30:16 -0300

    add loading..

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
+import Spinner from './components/spinner/spinner';
 
 class App extends React.Component {
 
    state = {
         resp: "",
-        msg: ""
+        msg: "",
+        isLoading: false
    }
 
    render() {
+
+        const { isLoading } = this.state;
+
         return (
+        <React.Fragment>
             <div className="upload-container">
                 <div className="upload-header">
                     <h2>Json Parser Example</h2>
@@ -21,6 +27,7 @@ class App extends React.Component {
                             <input id="singleFileUploadInput" type="file" name="file" className="file-input" required />
                             <button type="submit" className="primary submit-btn" onClick={this.submitFile}>Submit</button>
                         </div>
+                        {isLoading && <Spinner />}
                         <div className="upload-response">
                             <div id="singleFileUploadMsg">{this.state.msg}</div>
                             <p id="resposta">{this.state.resp}</p>
@@ -28,19 +35,24 @@ class App extends React.Component {
                     </div>
                 </div>
             </div>
+        </React.Fragment>
         );
     }
 
     submitFile = () => {
+
         var files = document.querySelector('#singleFileUploadInput').files;
         if(files.length !== 0) {
            this.uploadSingleFile(files[0]);
         }else{
             this.setState({resp:"",msg: "Favor selecionar um arquivo!"});
         }
+
     }
 
     uploadSingleFile(file) {
+
+        this.setState({ isLoading: true ,msg: ""});
 
         var formData = new FormData();
         formData.append("file", file);
@@ -51,17 +63,18 @@ class App extends React.Component {
 
         //depois da chamada
         xhr.onload = function() {
+            this.setState({ isLoading: true });
             var response = JSON.parse(xhr.responseText);
             console.log(response);
             if(xhr.status === 200) {
-                this.setState({resp:xhr.responseText,msg: "Arquivo lido corretamente"});
+                this.setState({resp:xhr.responseText,msg: "Arquivo lido corretamente",isLoading: false});
             } else {
-                this.setState({resp:"",msg:(response && response.message) || "Ocorreu algum erro"});
+                this.setState({resp:"",msg:(response && response.message) || "Ocorreu algum erro",isLoading: false});
             }
         }.bind(this);
 
         xhr.onerror = function () {
-           this.setState({ resp:"",msg:"Problema na chamada para api" });
+           this.setState({ resp:"",msg:"Problema na chamada para api" ,isLoading: false});
         }.bind(this);
 
         xhr.send(formData);
---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Spinner from 'react-bootstrap-spinner'
+
+export default () => (
+  <Spinner type="border" color="primary" size="5" />
+);
